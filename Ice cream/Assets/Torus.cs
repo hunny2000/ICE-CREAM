85a2f3a064d6d1890715f944b75ded0dfa258657
hunny2000 <abhinavbhasin58@gmail.com>
2020-04-19 00:05:25 +0530

    Ice_cream

---
+++ Ice cream/Assets/Torus.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Torus : MonoBehaviour
+{
+    public Material Choclate;
+    public Material Strawberry;
+    public Material Vanilla;
+
+    private static float Pi = 3.14159f;
+    public GameObject Cyl;
+    public float segmentRadius = 1f;
+    public float tubeRadius = 0.1f;
+    public int segments = 32;
+    public int tubes = 12;
+    public bool Pressed = false;
+
+    public List<Vector3> Pos;
+    public int Counter;
+    public int CurPos = 0;
+
+    //Create float for X Y Z coordinayes
+    float x = 0;
+    float y = 0;
+    float z = 0;
+    float Z = 0;
+    float X = 0;
+
+    private void Awake()
+    {
+        torus();
+        Counter = Pos.Count;
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Cyl.GetComponent<MeshRenderer>().material = Choclate;
+            Spawn();
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Cyl.GetComponent<MeshRenderer>().material = Strawberry;
+            Spawn();
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Cyl.GetComponent<MeshRenderer>().material = Vanilla;
+            Spawn();
+        }
+    }
+    public void Spawn()
+    {
+        Pos[CurPos] = new Vector3(Pos[CurPos].x, Pos[CurPos].y, X + Pos[CurPos].z);
+
+        GameObject C = GameObject.Instantiate(Cyl, Pos[CurPos], Quaternion.Euler(0, 0, Z), this.transform);
+        
+        Z += 3f;
+        X += 0.007f;
+
+        CurPos += 1;
+    }
+
+    public void torus()
+    {
+
+        // Calculate size of segment and tube
+        float segmentSize = 2 * Pi / segments;
+        float tubeSize = 2 * Pi / tubes;
+
+        // Loop through number of tubes
+        for (int H = 0; H < 7; H++)
+        {
+            {
+                for (int i = 0; i < segments; i++)
+                {
+                    for (int j = 0; j < tubes; j++)
+                    {
+                        // Calculate X, Y, Z coordinates.
+                        x = (segmentRadius + tubeRadius * Mathf.Cos(j * tubeSize)) * Mathf.Cos(i * segmentSize);
+                        y = (segmentRadius + tubeRadius * Mathf.Cos(j * tubeSize)) * Mathf.Sin(i * segmentSize);
+                        z = tubeRadius * Mathf.Sin(j * tubeSize);
+
+                    }
+                    Pos.Add(new Vector3(x, y, z));
+                }
+            }
+            segmentRadius -= 0.3f;
+        }
+        //this.transform.Rotate(Vector3.right, -90f);
+    }
+}
---
+++ Ice cream/Assets/UI.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public GameObject Cyl;
+    public Torus Cream;
+    public Material Choclate;
+    public Material Strawberry;
+    public Material Vanilla;
+
+    public Texture ChocolateTexture;
+    public Texture StrawberryTexture;
+    public Texture VanillaTexture;
+
+    public void Chocolate()
+    {
+        Cyl.GetComponent<MeshRenderer>().material = Choclate;
+        Cream.Spawn();
+    }
+    public void Straw()
+    {
+        Cyl.GetComponent<MeshRenderer>().material = Strawberry;
+        Cream.Spawn();
+    }
+    public void Vani()
+    {
+        Cyl.GetComponent<MeshRenderer>().material = Strawberry;
+        Cream.Spawn();
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.RepeatButton(MR(0.80f, 0.10f, 0.10f, 0.20f), ChocolateTexture))
+        {
+            Cyl.GetComponent<MeshRenderer>().material = Choclate;
+            Cream.Spawn();
+            Debug.Log("Clicked the button with an image");
+
+        }
+        if (GUI.RepeatButton(MR(0.80f, 0.40f, 0.10f, 0.20f), StrawberryTexture))
+        {
+            Cyl.GetComponent<MeshRenderer>().material = Strawberry;
+            Cream.Spawn();
+            Debug.Log("Clicked the button with an image");
+
+        }
+        if (GUI.RepeatButton(MR(0.80f, 0.70f, 0.10f, 0.20f), VanillaTexture))
+        {
+            Cyl.GetComponent<MeshRenderer>().material = Vanilla;
+            Cream.Spawn();
+            Debug.Log("Clicked the button with an image");
+
+        }
+
+    }
+    Rect MR(float x, float y, float w, float h)
+    {
+        return new Rect(Screen.width * x, Screen.height * y, Screen.width * w, Screen.height * h);
+    }
+}
+
